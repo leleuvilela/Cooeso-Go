b30a5b8a482f593118e4d4d3200d2f53478ac21e
Leleu Vilela <viniciusvilela19@gmail.com>
2017-04-20 13:20:12 -0300

    up

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>Congresso de Oftalmologia da UFG</title>
 	@if(Config::get('app.debug'))
 		<link href="{{ asset('build/css/app.css') }}" rel="stylesheet">
 		<link href="{{ asset('build/css/components.css') }}" rel="stylesheet">
